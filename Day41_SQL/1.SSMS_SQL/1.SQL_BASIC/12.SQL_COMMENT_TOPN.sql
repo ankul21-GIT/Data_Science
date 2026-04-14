534123e888570bf3f8b3005f5a9b8ff6ae1e6e3f
Ankul Singh <ankulsingh2151@gmail.com>
2026-04-14 14:30:09 +0530

    Comment TopN

---
+++ Day41_SQL/1.SSMS_SQL/1.SQL_BASIC/12.SQL_COMMENT_TOPN.sql
@@ -0,0 +1,20 @@
+-- Hi we are learning SQL Server
+
+
+SELECT * FROM dbo.Employees
+
+
+/* Hi
+we 
+are
+learning
+SQL Server */
+
+
+SELECT TOP 2 * FROM dbo.Employees
+
+
+SELECT TOP 2 EmployeeID, Salary FROM dbo.Employees
+
+
+SELECT TOP 5 FirstName,LastName FROM dbo.employees
