1a9dee59a88287984afe9c7a9e53d97d04e2ebd0
Ankul Singh <ankulsingh2151@gmail.com>
2026-05-04 07:47:43 +0530

    LEAD & LAG

---
+++ Day41_SQL/1.SSMS_SQL/2.SQL_FUNCTION/5.SQL_WINDOW_FUNCTION_LEAD_FUNCTION.sql
@@ -0,0 +1,46 @@
+
+CREATE DATABASE [Profit_DB]
+
+USE [profit_DB]
+
+-- Create a table to store monthly profit data for different products
+CREATE  TABLE ProfitData (
+    MonthNumber INT,
+    MonthName VARCHAR(3),
+    Product VARCHAR(50),
+    Profit INT
+);
+
+-- Insert data into the ProfitData table
+
+INSERT INTO ProfitData (MonthNumber, MonthName, Product, Profit) VALUES
+(1, 'Jan', 'Product A', 1000),
+(2, 'Feb', 'Product A', 1500),
+(3, 'Mar', 'Product A', 1200),
+(4, 'Apr', 'Product A', 1700),
+(5, 'May', 'Product A', 1300),
+(6, 'Jun', 'Product A', 1600),
+(1, 'Jan', 'Product B', 2000),
+(2, 'Feb', 'Product B', 2500),
+(3, 'Mar', 'Product B', 2200),
+(4, 'Apr', 'Product B', 2700),
+(5, 'May', 'Product B', 2300),
+(6, 'Jun', 'Product B', 2600);
+
+
+
+SELECT * FROM ProfitData
+
+--To add a new column that shows next month's profit for each product
+
+SELECT *,LEAD(Profit) OVER(PARTITION BY product ORDER BY monthnumber) AS [Next Month's Profit]
+FROM ProfitData
+
+--We don't want product column inthe output but we want each month's total profit to be show by monthnumber & monthname,Also a 
+--new column should be added to show next month's total profit
+
+SELECT monthnumber,monthname,sum(profit) AS [Total Profit],
+LEAD(SUM(profit)) OVER(ORDER BY monthnumber ASC) AS [Next Month's Total Profit]
+FROM ProfitData
+GROUP BY MonthNumber,MonthName
+ORDER BY MonthNumber
