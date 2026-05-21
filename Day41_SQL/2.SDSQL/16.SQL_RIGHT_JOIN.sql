cdd9725b6f7ee3df00dbbfcb7369dba8ec1d431a
Ankul Singh <ankulsingh2151@gmail.com>
2026-05-21 14:11:13 +0530

    RIGHT JOIN

---
+++ Day41_SQL/2.SDSQL/16.SQL_RIGHT_JOIN.sql
@@ -0,0 +1,55 @@
+/*
+RIGHT JOIN -> Return all the record from the right table, and mathc record from the right table.
+*/
+
+CREATE DATABASE college;
+
+USE college;
+
+
+CREATE TABLE student(
+	id INT PRIMARY KEY,
+    name VARCHAR(50)
+
+);
+
+INSERT INTO student
+(id, name)
+VALUES
+(101, "adam"),
+(102, "bob"),
+(103, "casey");
+
+
+CREATE TABLE course(
+	id INT PRIMARY KEY,
+    course VARCHAR(50)
+
+);
+
+INSERT INTO course
+(id, course)
+VALUES
+(102, "english"),
+(105, "math"),
+(103, "science"),
+(107, "computer science");
+
+
+
+SELECT * FROM student;
+
+SELECT * FROM course;
+
+
+SELECT * 
+FROM student
+RIGHT JOIN course
+ON student.id = course.id;
+
+-- Using alias
+
+SELECT * 
+FROM student as s
+RIGHT JOIN course as c
+ON s.id = c.id;
