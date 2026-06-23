e85b603bcdc0e8ea030c8e4b95e30733dc45a1a1
Ankul Singh <ankulsingh2151@gmail.com>
2026-06-23 12:55:52 +0530

    SQL

--- Day41_SQL/5.SQL_Interview/1.SQL_Q&A/1.SQL_Interview_Question.sql
+++ Day41_SQL/5.SQL_Interview/1.SQL_Q&A/1.SQL_Interview_Question.sql
@@ -1,7 +1,17 @@
 -- Here are 100+ MySQL interview questions with their answers, ranging from basic to more intermediate topics:
 
 
--- How do you create a new database in MySQL?
+-- Q1. How do you create a new database in MySQL?
 -- Answer
 
 CREATE DATABASE database_name;
+
+
+-- Q2. How do you create a new table in MySQL?
+
+CREATE TABLE table_name (
+    column1 datatype1,
+    column2 datatype2,
+    ...
+);
+
