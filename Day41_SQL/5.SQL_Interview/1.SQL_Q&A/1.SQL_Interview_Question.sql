a1af1e7f11abb1154b22aa71da6495af19c623f4
Ankul Singh <ankulsingh2151@gmail.com>
2026-06-23 12:51:56 +0530

    SQL

---
+++ Day41_SQL/5.SQL_Interview/1.SQL_Q&A/1.SQL_Interview_Question.sql
@@ -0,0 +1,7 @@
+-- Here are 100+ MySQL interview questions with their answers, ranging from basic to more intermediate topics:
+
+
+-- How do you create a new database in MySQL?
+-- Answer
+
+CREATE DATABASE database_name;
