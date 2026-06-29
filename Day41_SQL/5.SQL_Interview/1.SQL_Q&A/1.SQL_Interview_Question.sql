da2f27133cb45ec9e1875ee0d46a897b952ef21e
Ankul Singh <ankulsingh2151@gmail.com>
2026-06-29 14:20:49 +0530

    SQL

--- Day41_SQL/5.SQL_Interview/1.SQL_Q&A/1.SQL_Interview_Question.sql
+++ Day41_SQL/5.SQL_Interview/1.SQL_Q&A/1.SQL_Interview_Question.sql
@@ -80,6 +80,9 @@ SET column1 = value1, column2 = value2, ...
 WHERE some_column = some_value;
 
 
+-- Q15. How can you delete records from a table?
+-- Answer : DELETE FROM table_name WHERE condition;
+
 
 
 
