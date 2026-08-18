0dfd7bea16720f62c1fa654e58f6e648af812cf1
Ankul Singh <ankulsingh2151@gmail.com>
2026-08-18 22:37:57 +0530

    SQL

--- Day41_SQL/5.SQL_Interview/1.SQL_Q&A/2.SQL_Interview_Questions.sql
+++ Day41_SQL/5.SQL_Interview/1.SQL_Q&A/2.SQL_Interview_Questions.sql
@@ -8,3 +8,22 @@ WHERE N-1 = (SELECT COUNT(DISTINCT salary)
              FROM employee e2 
              WHERE e2.salary > e1.salary);
              
+             
+-- Q2. Explain `JOIN` and different types of `JOIN` in MySQL.
+-- Answer :
+/*JOIN is used to combine rows from two or more tables based on a related column between them. Types of joins:
+
+- INNER JOIN: Returns records that have matching values in both tables.
+- LEFT JOIN (or LEFT OUTER JOIN): Returns all records from the left table, and the matched records from the right table.
+- RIGHT JOIN (or RIGHT OUTER JOIN): Returns all records from the right table, and the matched records from the left table.
+- FULL JOIN (or FULL OUTER JOIN): Returns all records when there's a match in one of the tables.
+*/
+
+
+
+
+
+
+
+
+             
